2712cabf68eb070a7e6f384dde1d07bab3979e67
Benjamin Metzler <4074316+benjaminmetzler@users.noreply.github.com>
2021-05-12 21:49:34 -0700

    move stb to IR

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd /home/pi/my_remote
-source venv/bin/activate
-sudo python3 scripts/main.py
+source /home/pi/my_remote/venv/bin/activate
+sudo /home/pi/my_remote/venv/bin/python3 scripts/main.py
+# sudo python3 scripts/keyboard_loop.py
