d6ad064e36b989f5a9de301c303aab1b9ff51354
Benjamin Metzler <4074316+benjaminmetzler@users.noreply.github.com>
2022-05-21 16:40:42 -0700

    pylinting

--- scripts/main.py
+++ scripts/main.py
@@ -1,20 +1,25 @@
+'''my_remote'''
 import json
-import keyboard
 import os
 import pathlib
 import time
+import keyboard
 import lirc
 
 
 class My_Remote:
+    '''my_remote'''
     def __init__(self, conf_file):
+        '''__init__(self, conf_file):'''
         self.mode = {}
         self.current_mode_file = ""
         self.load(conf_file)
         self.key_presses = {}
         self.client = lirc.Client()
 
-    def process_code(self, code, long_press):
+    # @staticmethod
+    def process_code(self, code, long_press): # pylint: disable=too-many-branches
+        '''process_code(self, code, long_press):'''
         if long_press and "long_press" in code:
             code = code["long_press"]
         if "type" in code:
@@ -22,7 +27,8 @@ class My_Remote:
                 repeat = code["repeat"]
             else:
                 repeat = 1
-            for x in range(repeat):
+            for repeat_count in range(repeat):
+                print(f"Try {repeat_count}")
                 if code["type"] == "ir":
                     self.send_ir(code["device"], code["code"])
                 elif code["type"] == "bluetooth":
@@ -38,13 +44,17 @@ class My_Remote:
                 elif code["type"] == "macro":
                     for macro_code in code["macro"]:
                         self.process_code(macro_code, long_press)
+                elif code["type"] == "appletv":
+                    self.appletv(code["device"], code["code"])
                 else:
-                    print("Unknown type(%s)" % self.code["type"])
+                    print(f'Unknown type({code["type"]})')
 
         else:
-            print("Type not found: %s" % code)
+            print(f"Type not found: {code}" )
 
+    # @staticmethod
     def load(self, conf_file):
+        '''load(self, conf_file):'''
         file = pathlib.Path(conf_file)
         if file.exists():
 
@@ -53,14 +63,13 @@ class My_Remote:
             # read the common file
             # TK should we have an on_load and on_unload in the common.json?
             # TK how should they be handled?
-            f = open("/home/pi/my_remote/json/common.json")
-            self.common = json.load(f)
-            f.close()
+            with open("/home/pi/my_remote/json/common.json", "r", encoding="utf8") as file_handle:
+                self.common = json.load(file_handle)
+
 
             # read the configuration file
-            f = open(conf_file)
-            self.mode = json.load(f)
-            f.close()
+            with open(conf_file, "r", encoding="utf8") as file_handle:
+                self.mode = json.load(file_handle)
 
             self.mode.update(self.common)
 
@@ -68,54 +77,72 @@ class My_Remote:
             # load up the defaults
             self.on_load()
         else:
-            print("Unable to find: %s" % conf_file)
+            print(f"Unable to find: {conf_file}")
 
+    # @staticmethod
     def send_ir(self, device, code):
-        command = 'irsend SEND_ONCE "%s" "%s"' % (device, code)
-        print("%s | %s" % (device, command))
+        '''send_ir(self, device, code):'''
+        # command = f'irsend SEND_ONCE "{device}" "{code}"'
+        print(f"{device} | {code}")
         try:
             self.client.send_once(device, code)
         except lirc.exceptions.LircdCommandFailureError as error:
-            print("Unable to send the %s key to %s!" % (device, code))
+            print(f"Unable to send the {device} key to {code}!")
             print(error)  # Error has more info on what lircd sent back.
             # os.system(command)
 
+    # @staticmethod
     def adb(self, device, code):
+        '''adb(self, device, code):'''
         if code == "CONNECT":
-            command = "adb connect %s" % device
+            command = f"adb connect {device}"
         elif code == "DISCONNECT":
             command = "adb disconnect"
         else:
-            command = 'adb shell input keyevent "%s"' % code
-        print("%s | %s" % (device, command))
+            command = f'adb shell input keyevent "{code}"'
+        print(f"{device} | {code}")
         os.system(command)
 
+    # @staticmethod
+    def appletv(self, device, code):
+        '''appletv(self, device, code):'''
+        command = f"TBD: {device} --> AppleTV({code})"
+        print(f"{device} | {command}")
+
+    # @staticmethod
     def curl(self, device, code):
-        command = 'curl "%s" "%s"' % (code, device)
-        print("%s | %s" % (device, command))
+        '''curl(self, device, code):'''
+        command = f'curl "{code}" "{device}"'
+        print(f"{device} | {code}")
         os.system(command)
 
+    # @staticmethod
     def bluetooth(self, device, code):
-        command = "TBD: %s --> bluetooth(%s)" % (device, code)
-        print("%s | %s" % (device, command))
+        '''bluetooth(self, device, code):'''
+        command = f"TBD: {device} --> bluetooth({code})"
+        print(f"{device} | {command}")
         # os.system("TK")
 
+    # @staticmethod
     def sleep(self, device, duration):
-        command = 'sleep "%s"' % duration
-        print("%s | %s" % (device, command))
+        '''sleep(self, device, duration):'''
+        command = f'sleep "{duration}"'
+        print(f"{device} | {command}")
         os.system(command)
 
+    # @staticmethod
     def callback_key_release(self, event):
+        '''callback_key_release(self, event):'''
         long_press = False
         scan_code = str(event.scan_code)
         name = event.name
-        print("callback_key_release: %s - %s (%s)" % (event, scan_code, name))
+        print(f"callback_key_release: {event} - {scan_code} ({name})" )
         if scan_code in self.mode:
             if scan_code in self.key_presses:
                 current_time = time.time()
                 time_diff = current_time - self.key_presses[scan_code]
                 del self.key_presses[scan_code]
-                print("key_press_duration=%s" % time_diff)
+                print(f"key_press_duration={time_diff}")
 
                 # NOTE: If the time of a keypress is greater than 1 second
                 # then handle this as a long press
@@ -124,31 +151,39 @@ class My_Remote:
 
                 self.process_code(self.mode[scan_code], long_press)
 
+    # @staticmethod
     def callback_key_press(self, event):
+        '''callback_key_press(self, event):'''
         # This callback adds the keypress to the dictionary along
         # with the current time so that when the key is released
         # the time can be used to determine if it was a long
         # or short press, allowing different actions for the same key.
         scan_code = str(event.scan_code)
         name = event.name
-        print("callback_key_press: %s - %s (%s)" % (event, scan_code, name))
+        print(f"callback_key_press: {event} - {scan_code} ({name})")
         if scan_code in self.mode:
             if scan_code not in self.key_presses:
                 self.key_presses[scan_code] = time.time()
             print(self.key_presses)
 
+    # @staticmethod
     def on_load(self):
-        print("on_load: %s" % self.current_mode_file)
+        '''on_load(self):'''
+        print(f"on_load: {self.current_mode_file}")
         if "on_load" in self.mode:
             self.process_code(self.mode["on_load"], False)
 
+    # @staticmethod
     def on_unload(self):
-        print("on_unload: %s" % self.current_mode_file)
+        '''on_unload(self):'''
+        print(f"on_unload: {self.current_mode_file}")
         if "on_unload" in self.mode:
             self.process_code(self.mode["on_unload"], False)
         self.mode = {}
 
+    # @staticmethod
     def event_loop(self):
+        '''event_loop(self):'''
         keyboard.on_press(callback=self.callback_key_press, suppress=True)
         keyboard.on_release(callback=self.callback_key_release, suppress=True)
         keyboard.wait()
