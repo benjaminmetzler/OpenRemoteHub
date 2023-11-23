029189302b47a9153f318c0064755dd879fe469c
Benjamin Metzler <4074316+benjaminmetzler@users.noreply.github.com>
2023-11-22 20:59:51 -0800

    Update install for Newer Raspian Version (#13)
    1. No longer need to compile lirc as it works out of the box with the
       anavi hat
    2. Update README.md to show new tested version
    3. General cleanup

---
+++ documentation/smart_remote_pairing.md
@@ -0,0 +1,6 @@
+# Using a Logitech Unifi Remote
+
+* Install ltunify on the pi: `sudo apt install ltunify`
+* Enter pairing mode on the pi with: `sudo ltunify pair`
+* On the Smart Remote, enable pairing mode by pressing the Menu & Mute buttons at the same time
+* Verify device is paired: `sudo ltunify list`
--- setup_pi4.sh
+++ setup_pi4.sh
@@ -1,59 +1,26 @@
 #! /bin/bash
-# https://github.com/AnaviTechnology/anavi-docs/blob/master/anavi-infrared-phat/anavi-infrared-phat.md#infrared-and-lirc
 
 currentdir=$(pwd)
 
-echo "*** add the source files to apt ***"
-sudo su -c "grep '^deb ' /etc/apt/sources.list | sed 's/^deb/deb-src/g' > /etc/apt/sources.list.d/deb-src.list"
-
 echo "*** Install the necessary packages ***"
 sudo apt update
-sudo apt upgrade -y
-
 sudo apt install -y git \
                     vim \
-                    devscripts \
-                    dh-exec \
-                    doxygen \
-                    expect \
-                    libasound2-dev \
-                    libftdi1-dev \
-                    libsystemd-dev \
-                    libudev-dev \
-                    libusb-1.0-0-dev \
-                    libusb-dev \
-                    man2html-base \
-                    portaudio19-dev \
-                    socat \
-                    xsltproc \
-                    python3-yaml \
-                    dh-python \
-                    libx11-dev \
-                    python3-dev \
-                    python3-setuptools \
+                    python3 \
                     python3-pip \
-                    adb
+                    virtualenv \
+                    evtest \
+                    lirc
 
-echo "*** Build the patched version of lirc for the anava IR adapter ***"
-mkdir $currentdir/lirc-src
-cd $currentdir/lirc-src
-apt source lirc
-wget https://raw.githubusercontent.com/neuralassembly/raspi/master/lirc-gpio-ir-0.10.patch
-patch -p0 -i lirc-gpio-ir-0.10.patch -R
-cd lirc-0.10.1
-debuild -uc -us -b
-cd $currentdir/lirc-src
-sudo apt install -y ./liblirc0_0.10.1-6.3_armhf.deb ./liblircclient0_0.10.1-6.3_armhf.deb ./lirc_0.10.1-6.3_armhf.deb
+echo "*** Configure kernal extensions for the anava IR adapter ***"
+sudo su -c "echo 'dtoverlay=gpio-ir,gpio_pin=18' >> /boot/config.txt"
+sudo su -c "echo 'dtoverlay=gpio-ir-tx,gpio_pin=17' >> /boot/config.txt"
 
 echo "*** Update the lirc to support the anava IR adapter ***"
 sudo cp /etc/lirc/lirc_options.conf /etc/lirc/lirc_options.conf.backup.$(date '+%Y%m%d%H%M%S')
 sudo su -c "cat /etc/lirc/lirc_options.conf | sed 's/devinput/default/g' > /etc/lirc/lirc_options.conf.new"
 sudo su -c "cat /etc/lirc/lirc_options.conf.new | sed 's/auto/\/dev\/lirc0/g' > /etc/lirc/lirc_options.conf"
 sudo rm /etc/lirc/lirc_options.conf.new
-
-echo "*** Configure kernal extensions for the anava IR adapter ***"
-sudo su -c "echo 'dtoverlay=gpio-ir,gpio_pin=18' >> /boot/config.txt"
-sudo su -c "echo 'dtoverlay=gpio-ir-tx,gpio_pin=17' >> /boot/config.txt"
 
 echo "*** enable the lircd service ***"
 sudo systemctl enable lircd
@@ -62,31 +29,19 @@ echo "*** Prevent power buttons on remotes from putting the Raspberry Pi to slee
 sudo su -c "echo 'HandlePowerKey=ignore' >> /etc/systemd/logind.conf"
 sudo su -c "echo 'HandleSuspendKey=ignore' >> /etc/systemd/logind.conf"
 
-echo "*** Disable tty session to prevent the keyboard access from locking the system ***"
-sudo su -c "echo 'NAutoVTs=0' >> /etc/systemd/logind.conf"
-sudo su -c "echo 'eserveVT=0' >> /etc/systemd/logind.conf"
-systemctl disable getty@tty1.service
-
 echo "*** pull the OpenRemoteHub repo locally ***"
 cd $currentdir
 git clone https://github.com/benjaminmetzler/OpenRemoteHub.git
 cd $currentdir/OpenRemoteHub
-sudo pip3 install virtualenv
 virtualenv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-
-echo "*** Copy and activate the harmony companion map for unknown keys ***"
-cd $currentdir/OpenRemoteHub
-sudo cp scripts/98-harmonycompanion.hwdb /etc/udev/hwdb.d/
-sudo systemd-hwdb update
-sudo udevadm trigger
 
 echo "*** copy the sample lirc IR defintion files ***"
 cd $currentdir/OpenRemoteHub
 sudo cp ir_database/*.conf /etc/lirc/lircd.conf.d/
 
-echo "*** Install and enable the OpenRemoteHub service ***"
+# echo "*** Install and enable the OpenRemoteHub service ***"
 # sudo cp OpenRemoteHub.service /etc/systemd/system/OpenRemoteHub.service
 # sudo systemctl enable OpenRemoteHub
 
